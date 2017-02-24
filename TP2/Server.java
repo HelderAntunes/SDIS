1bd81b04e8cd67b852e9c4e183276da274d2d459
HelderAntunes <helder_antunes-@hotmail.com>
2017-02-24 22:36:21 +0000

    Create thread in server side

--- TP2/Server.java
+++ TP2/Server.java
@@ -1,42 +1,48 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.MulticastSocket;
 import java.net.InetAddress;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Server {
-	
-    /*java Server <srvc_port> <mcast_addr> <mcast_port>
-   where:
 
-       <srvc_port> is the port number where the server provides the service
-       <mcast_addr> is the IP address of the multicast group used by the server to advertise its service.
-       <mcast_port> is the multicast group port number used by the server to advertise its service.   
-	*/
-	  
-	public static void main(String[] args) throws IOException, InterruptedException {
-		int serverPort = Integer.parseInt(args[0]); // 2001
-		String mcast_addr = args[1]; // "224.0.0.3"
-		int mcast_port = Integer.parseInt(args[2]); // 2000
-		
-		
-		InetAddress addr = InetAddress.getByName(mcast_addr);
-		
-		try (DatagramSocket serverSocket = new DatagramSocket(serverPort)) {
-            for (int i = 0; i < 5; i++) {
-                String msg = "Sent message no " + i;
+	public Server(String mcast_addr, int mcast_port, int serverPort) {
+		new Timer().schedule(new RemindTask(mcast_addr, mcast_port, serverPort), 0, 1000);
+	}
 
-               
-                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
-                msg.getBytes().length, addr, mcast_port);
-                
-                serverSocket.send(msgPacket);
-     
-                System.out.println("Server sent packet with msg: " + msg);
-                Thread.sleep(500);
+	public static void main(String[] args) {
+        // Invocation: java Server <srvc_port> <mcast_addr> <mcast_port>
+		Server server = new Server(args[1], Integer.parseInt(args[2]), Integer.parseInt(args[0]));
+	}
+
+	class RemindTask extends TimerTask {
+
+        private int mcast_port;
+        private MulticastSocket serverSocket;
+        private InetAddress mcast_addr;
+
+		public RemindTask(String mcast_addr, int mcast_port, int serverPort) {
+            try {
+                this.mcast_port = mcast_port;
+                this.serverSocket = new MulticastSocket(serverPort);
+                this.mcast_addr = InetAddress.getByName(mcast_addr);
             }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
+		}
+
+        public void run() {
+    		try {
+                String msg = "message";
+                serverSocket.send(new DatagramPacket(msg.getBytes(), msg.getBytes().length, this.mcast_addr, this.mcast_port));
+                System.out.println("Server sent packet with msg: " + msg);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+        	}
         }
-    }
-	
+	}
+
 }
